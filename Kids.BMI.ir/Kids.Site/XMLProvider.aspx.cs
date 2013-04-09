1c51c4a588f0fc6ed7937ff53fbf2459f02df308
Sina Aslani/n <sina.aslani@gmail.com/n>
2013-04-09 18:27:51 +0430

    تست سینا

--- Kids.BMI.ir/Kids.Site/XMLProvider.aspx.cs
+++ Kids.BMI.ir/Kids.Site/XMLProvider.aspx.cs
@@ -9,7 +9,7 @@ namespace Site.Kids.bmi.ir
     {
         private enum XMLType
         {
-            Profile_BankStory = 1,22
+            Profile_BankStory = 1,258
         }
 
         protected void Page_Load(object sender, EventArgs e)
